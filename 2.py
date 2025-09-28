b27885acb44a9e73a4009b41a641d1c7e91dbb31
kartikayjain20 <kartikayjain123@gmail.com>
2025-09-28 17:40:30 +0530

    kmk

--- 2.py
+++ 2.py
@@ -1,7 +1,7 @@
 import math
 def area_circle(radius):
     return math.pi*radius**2
-def area_rectange(length,breadth):
+def area_rectangle(length,breadth):
     return length*breadth
 def area_triangle(base,height):
     return 0.5*base*height
@@ -16,8 +16,8 @@ def area_parallelogram(base,height):
 radius=float(input("Enter the radius of the circle: "))
 print(f"Area of circle: {area_circle(radius)}")
 length=float(input("Enter the length of the rectangle: "))
-breadth=float(input("Enter the breadth of the rectange: "))
-print(f"Area of square: {area_rectange(length,breadth)}")
+breadth=float(input("Enter the breadth of the rectangle: "))
+print(f"Area of square: {area_rectangle(length,breadth)}")
 base=float(input("Enter the base of the triangle: "))
 height1=float(input("Enter the height of the triangle"))
 print(f"Area of triangle: {area_triangle(base,height1)}")
